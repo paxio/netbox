c9b7879878eaba3aec03a843f6eab354a34c31ec
Arnoud Vermeer <avermeer@paxio.com>
2018-05-15 11:08:04 +0200

    Add VLANGroup to nestedVLAN

--- netbox/ipam/api/serializers.py
+++ netbox/ipam/api/serializers.py
@@ -180,10 +180,11 @@ class VLANSerializer(CustomFieldModelSerializer):
 
 class NestedVLANSerializer(serializers.ModelSerializer):
     url = serializers.HyperlinkedIdentityField(view_name='ipam-api:vlan-detail')
+    group = NestedVLANGroupSerializer()
 
     class Meta:
         model = VLAN
-        fields = ['id', 'url', 'vid', 'name', 'display_name']
+        fields = ['id', 'group', 'url', 'vid', 'name', 'display_name']
 
 
 class WritableVLANSerializer(CustomFieldModelSerializer):
