cdfdf92c00b74d93534d0ae7c8b3ec18823da835
Arnoud Vermeer <avermeer@paxio.com>
2018-10-17 09:37:36 +0200

    Fix changelog model urls for tenants

--- netbox/tenancy/urls.py
+++ netbox/tenancy/urls.py
@@ -15,6 +15,7 @@ urlpatterns = [
     url(r'^service-providers/import/$', views.TenantGroupBulkImportView.as_view(), name='tenantgroup_import'),
     url(r'^service-providers/delete/$', views.TenantGroupBulkDeleteView.as_view(), name='tenantgroup_bulk_delete'),
     url(r'^service-providers/(?P<slug>[\w-]+)/edit/$', views.TenantGroupEditView.as_view(), name='tenantgroup_edit'),
+    url(r'^service-providers/(?P<slug>[\w-]+)/changelog/$', ObjectChangeLogView.as_view(), name='tenantgroup_changelog', kwargs={'model': TenantGroup}),
 
     # Tenants
     url(r'^customers/$', views.TenantListView.as_view(), name='tenant_list'),
@@ -25,6 +26,7 @@ urlpatterns = [
     url(r'^customers/(?P<slug>[\w-]+)/$', views.TenantView.as_view(), name='tenant'),
     url(r'^customers/(?P<slug>[\w-]+)/edit/$', views.TenantEditView.as_view(), name='tenant_edit'),
     url(r'^customers/(?P<slug>[\w-]+)/delete/$', views.TenantDeleteView.as_view(), name='tenant_delete'),
+    url(r'^customers/(?P<slug>[\w-]+)/changelog/$', ObjectChangeLogView.as_view(), name='tenant_changelog', kwargs={'model': Tenant}),
 
     # Packages
     url(r'^packages/$', views.PackageListView.as_view(), name='package_list'),
@@ -35,5 +37,6 @@ urlpatterns = [
     url(r'^packages/(?P<slug>[\w-]+)/$', views.PackageView.as_view(), name='package'),
     url(r'^packages/(?P<slug>[\w-]+)/edit/$', views.PackageEditView.as_view(), name='package_edit'),
     url(r'^packages/(?P<slug>[\w-]+)/delete/$', views.PackageDeleteView.as_view(), name='package_delete'),
+    url(r'^packages/(?P<slug>[\w-]+)/changelog/$', ObjectChangeLogView.as_view(), name='package_changelog', kwargs={'model': Package}),
 
 ]
